9166965ad4a101ffab8630b9da0f686fee9532fe
35298 <35298@DESKTOP-PLD6KM2>
2022-03-25 21:52:48 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Test2
+{
+    class Program
+    {
+        public static void grows( ref Student student)
+        {
+            //student = new Student();
+            //student.age++;
+        }
+        // static Student grow(Student student)
+        //{
+        //    Student sql = new Student();
+            
+        //     sql.age = student.age++;
+            
+        //    return sql;
+        //}
+        static void Main(string[] args)
+        {
+            //Student GZC = new Student("葛志超");
+            //Student sj = new Student(12);
+            //Student MJ = new Student();
+             //= new Student("aa",16);
+
+            //int a = 18;
+            //int b = a;
+            //b = 20;
+            //Console.WriteLine(b+a);
+            //Console.ReadLine();
+            //Student wx = new Student();
+            //Student wx = new Student();
+            //wx.age = 20;
+            //Student Clone = wx;
+            //Clone.age = 100;
+            //Console.WriteLine(wx.age);
+            //Student wx = new Student();
+            //wx.age = 18;
+            //grow(wx);
+            //ReadLine();
+            //Student aa = new Student();
+            //aa.age = 19;
+            //grow(aa);
+            //WriteLine(aa.age);
+            //grows(ref aa);
+            //WriteLine(aa.age);
+            //WriteLine();
+            //Student csharp = new Student();
+            //csharp.age = 19;
+            //csharp = grow(csharp);
+            //WriteLine(csharp.age);
+            //ReadLine();
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Test2
+{
+    class Student
+    {
+        public  int age;
+        public  string name;
+
+        public Student(int age) 
+        {
+       
+        
+        }
+        public Student()
+        {
+        
+
+        }
+
+        public Student(String name)
+        { 
+        WriteLine($"Hello{name}");
+        
+        }
+        public static void greet()
+        {
+
+            //int age = 18;
+            //age++;
+
+
+        }
+        public static void study() 
+        {
+         ///aaaaa
+        
+        }
+      
+    }
+    
+}
